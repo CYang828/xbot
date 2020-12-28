a5dba3c2ea6c4e1c9ba8c136434b36a114e1d8e5
zhengjiawei010 <zhengjiawei@kaikeba.com>
2020-12-28 21:15:44 +0800

    <feat>: <add cli commands test feature>

---
+++ tests/test_cli_command.py
@@ -0,0 +1,19 @@
+from src.xbot.cli.commands import BaseXbotCLICommand
+import pytest
+
+class MyTestCase(BaseXbotCLICommand):
+    def __init__(self):
+        super().__init__()
+        self.predict_parses.add_argument('--version', type=str, default='0.0.1', help='xbot version')
+
+
+#
+# def runner():
+#     return BaseXbotCLICommand()
+
+
+def test_main_succeeds():
+    subcommand = MyTestCase()
+    result = subcommand.parser.parse_args(['predict', '--version', '0.0.2'])
+    assert result.version == '0.0.2'
+
