5e894e906e3459d9ac6e72e45c58626df96a4f6d
zhengjiawei010 <zhengjiawei@kaikeba.com>
2020-12-28 21:39:41 +0800

    <refactor>: <refactor file location and name>

--- tests/cli/test_cli_command_succeed.py
+++ tests/cli/test_cli_command_succeed.py
@@ -1,15 +1,14 @@
 from src.xbot.cli.commands import BaseXbotCLICommand
 import pytest
 
+
 class MyTestCase(BaseXbotCLICommand):
     def __init__(self):
         super().__init__()
         self.predict_parses.add_argument('--version', type=str, default='0.0.1', help='xbot version')
 
 
-
 def test_main_succeeds():
     subcommand = MyTestCase()
     result = subcommand.parser.parse_args(['predict', '--version', '0.0.2'])
     assert result.version == '0.0.2'
-
