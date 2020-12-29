6ea5a303b31a196febfdc175cbeab0514bd42562
zhengjiawei010 <zhengjiawei@kaikeba.com>
2020-12-29 15:57:04 +0800

    <fix>: <fix BaseXbotCliCommand>

--- tests/cli/test_cli_command_succeed.py
+++ tests/cli/test_cli_command_succeed.py
@@ -1,11 +1,32 @@
-from src.xbot.cli.commands import BaseXbotCLICommand
+from src.xbot.cli.commands import BaseXbotCliCommand
 
 
-class MyTestCase(BaseXbotCLICommand):
+class MyTestCase(BaseXbotCliCommand):
     def __init__(self):
-        super().__init__()
-        self.predict_parses.add_argument('--version', type=str, default='0.0.1', help='xbot version')
+        super().__init__(command_dict)
+        self.sub_parser.add_argument('--version', type=str, default='0.0.1', help='xbot version')
 
+
+command_dict = {'train': {'param': [["--data_dir", str, './', "path to dataset."],
+                                    ['--output', str, "./", "path to saved the trained model."],
+                                    ["--batch_size", int, 32, "Batch size for training."],
+                                    ["--learning_rate", float, 3e-5, "Learning rate."],
+                                    ["--device", str, 'cuda:0', "train or test device"],
+                                    ["--seed", int, 2021, "random seed"],
+                                    ["--model", str, "hfl/chinese-bert-wwm-ext",
+                                     "Model's name or path to stored model."]
+                                    ]
+                          },
+                'predict': {'param': [["--data_dir", str, './', "path to dataset."],
+                                      ['--output', str, "./", "path to saved the trained model."],
+                                      ["--batch_size", int, 64, "Batch size for training."],
+                                      ["--learning_rate", float, 3e-9, "Learning rate."],
+                                      ["--device", str, 'cuda:0', "train or test device"],
+                                      ["--seed", int, 2021, "random seed"],
+                                      ["--model", str, "hfl/chinese-bert-wwm-ext",
+                                       "Model's name or path to stored model."]
+                                      ]
+                            }}
 
 def test_main_succeeds():
     subcommand = MyTestCase()
