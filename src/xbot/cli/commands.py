bd8312ed62284891d7adf5f10a7c6da132e451bb
zhengjiawei010 <zhengjiawei@kaikeba.com>
2021-01-05 21:04:02 +0800

    <refactor>: <refactor register_subcommand,test_main_succeeds>

--- src/xbot/cli/commands.py
+++ src/xbot/cli/commands.py
@@ -4,41 +4,33 @@
 # @FileName: commands.py
 # @Software: PyCharm
 import argparse
+from functools import wraps
 
 
-class BaseXbotCliCommand:
-    def __init__(self, *args) -> object:
-        pass
+# class BaseXbotCliCommand:
+#
+#     def __init__(self, *args) -> object:
+#         self.parser = argparse.ArgumentParser("Xbot CLI tool", usage="xbot cli <command> [<args>]")
+#         self.commands_parser = self.parser.add_subparsers(help="xbot-cli command helpers")
 
-    @staticmethod
-    def register_subcommand(parser, command_dict: dict) -> object:
-        """
-        Register this command to argparse so it's available for the transformer-cli
 
-        Args:
-            parser: Root parser to register command-specific arguments
-         command_dict = {'param': [["--data_dir", str, './', "path to dataset."],
-                              ['--output', str, "./", "path to saved the trained model."],
-                              ["--batch_size", int, 32, "Batch size for training."],
-                              ["--learning_rate", float, 3e-5, "Learning rate."],
-                              ["--device", str, 'cuda:0', "train or test device"],
-                              ["--seed", int, 2021, "random seed"],
-                              ["--model", str, "hfl/chinese-bert-wwm-ext",
-                               "Model's name or path to stored model."]
-                              ]
-                    }
-                    :param commands_parser:
-                    :param command_dict:
-        """
-        sub_parser = parser.add_parser(
-            list(command_dict.keys())[0],
-            help="xbot tool to {} a model on a task.".format(
-                list(command_dict.keys())[0]
-            ),
-        )
-        for each in command_dict.get(list(command_dict.keys())[0]):
-            sub_parser.add_argument(
-                each[0], type=each[1], default=each[2], help=each[3]
-            )
-        sub_parser.set_defaults()
-        return sub_parser
+class register_subcommand(object):
+    def __call__(self, fn):
+        @wraps(fn)
+        def wrapper(command_dict, *args, **kwargs):
+            self.fn = fn
+            fn_name = fn.__name__
+            parser = argparse.ArgumentParser("Xbot CLI tool", usage="xbot cli <command> [<args>]")
+            commands_parser = parser.add_subparsers(help="xbot-cli command helpers")
+            sub_parser = commands_parser.add_parser(fn_name,
+                                                    help="xbot tool to {} a model on a task.".format(
+                                                        fn_name))
+
+            for each in command_dict.get(fn_name):
+                sub_parser.add_argument(each[0], type=each[1], default=each[2], help=each[3])
+            sub_parser.set_defaults()
+            args_ = parser.parse_args([fn_name, '--seed', '2022'])
+            fn(command_dict, *args, **kwargs)
+            return args_
+
+        return wrapper
--- tests/cli/test_cli_command_succeed.py
+++ tests/cli/test_cli_command_succeed.py
@@ -1,124 +1,45 @@
-import argparse
-from src.xbot.cli.commands import BaseXbotCliCommand
+from src.xbot.cli.commands import register_subcommand
+
+command_dict_0 = {'train': [["--data_dir", str, './', "path to dataset."],
+                            ['--output', str, "./", "path to saved the trained model."],
+                            ["--batch_size", int, 32, "Batch size for training."],
+                            ["--learning_rate", float, 3e-5, "Learning rate."],
+                            ["--device", str, 'cuda:0', "train or test device"],
+                            ["--seed", int, 2021, "random seed"],
+                            ["--model", str, "hfl/chinese-bert-wwm-ext",
+                             "Model's name or path to stored model."]
+                            ]
+    ,
+                  'predict': [["--data_dir", str, './', "path to dataset."],
+                              ['--output', str, "./", "path to saved the trained model."],
+                              ["--batch_size", int, 64, "Batch size for training."],
+                              ["--learning_rate", float, 3e-9, "Learning rate."],
+                              ["--device", str, 'cuda:0', "train or test device"],
+                              ["--seed", int, 2021, "random seed"],
+                              ["--model", str, "hfl/chinese-bert-wwm-ext",
+                               "Model's name or path to stored model."]
+                              ]
+                  }
 
 
-# class MyTestCase(BaseXbotCliCommand):
-#     def __init__(self):
-#         super().__init__(command_dict)
-#         self.sub_parser.add_argument('--version', type=str, default='0.0.1', help='xbot version')
-#
-#
-# command_dict = {'train': {'param': [["--data_dir", str, './', "path to dataset."],
-#                                     ['--output', str, "./", "path to saved the trained model."],
-#                                     ["--batch_size", int, 32, "Batch size for training."],
-#                                     ["--learning_rate", float, 3e-5, "Learning rate."],
-#                                     ["--device", str, 'cuda:0', "train or test device"],
-#                                     ["--seed", int, 2021, "random seed"],
-#                                     ["--model", str, "hfl/chinese-bert-wwm-ext",
-#                                      "Model's name or path to stored model."]
-#                                     ]
-#                           },
-#                 'predict': {'param': [["--data_dir", str, './', "path to dataset."],
-#                                       ['--output', str, "./", "path to saved the trained model."],
-#                                       ["--batch_size", int, 64, "Batch size for training."],
-#                                       ["--learning_rate", float, 3e-9, "Learning rate."],
-#                                       ["--device", str, 'cuda:0', "train or test device"],
-#                                       ["--seed", int, 2021, "random seed"],
-#                                       ["--model", str, "hfl/chinese-bert-wwm-ext",
-#                                        "Model's name or path to stored model."]
-#                                       ]
-#                             }}
-#
-# def test_main_succeeds():
-#     subcommand = MyTestCase()
-#     result = subcommand.parser.parse_args(['predict', '--version', '0.0.2'])
-#     assert result.version == '0.0.2'
+class MyTestCase2:
 
-
-class MyTestCase1(BaseXbotCliCommand):
     def __init__(self, *args):
         super().__init__(*args)
 
     @staticmethod
-    def train():
-        print("正确运行")
-
-
-class MyTestCase2(BaseXbotCliCommand):
-    def __init__(self, *args):
-        super().__init__(*args)
+    @register_subcommand()
+    def train(command_dict2, list_) -> object:
+        print(list_)
 
     @staticmethod
-    def train():
-        print("正确运行")
-
-
-class MyTestCase3(BaseXbotCliCommand):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    @staticmethod
-    def train():
-        print("正确运行")
+    @register_subcommand()
+    def predict(command_dict1) -> object:
+        pass
 
 
 def test_main_succeeds():
-    command_dict1 = {
-        "train": [
-            ["--data_dir", str, "./", "path to dataset."],
-            ["--output", str, "./", "path to saved the trained model."],
-            ["--batch_size", int, 32, "Batch size for training."],
-            ["--learning_rate", float, 3e-5, "Learning rate."],
-            ["--device", str, "cuda:0", "train or test device"],
-            ["--seed", int, 2021, "random seed"],
-            [
-                "--model",
-                str,
-                "hfl/chinese-bert-wwm-ext",
-                "Model's name or path to stored model.",
-            ],
-        ]
-    }
-
-    command_dict2 = {
-        "test": [
-            ["--data_dir", str, "./", "path to dataset."],
-            ["--output", str, "./", "path to saved the trained model."],
-            ["--batch_size", int, 32, "Batch size for training."],
-            ["--learning_rate", float, 3e-5, "Learning rate."],
-            ["--device", str, "cuda:0", "train or test device"],
-            ["--seed", int, 2021, "random seed"],
-            [
-                "--model",
-                str,
-                "hfl/chinese-bert-wwm-ext",
-                "Model's name or path to stored model.",
-            ],
-        ]
-    }
-
-    command_dict3 = {
-        "predict": [
-            ["--data_dir", str, "./", "path to dataset."],
-            ["--output", str, "./", "path to saved the trained model."],
-            ["--batch_size", int, 32, "Batch size for training."],
-            ["--learning_rate", float, 3e-5, "Learning rate."],
-            ["--device", str, "cuda:0", "train or test device"],
-            ["--seed", int, 2021, "random seed"],
-            [
-                "--model",
-                str,
-                "hfl/chinese-bert-wwm-ext",
-                "Model's name or path to stored model.",
-            ],
-        ]
-    }
-    parser = argparse.ArgumentParser(
-        "Xbot CLI tool", usage="xbot cli <command> [<args>]"
-    )
-    commands_parser = parser.add_subparsers(help="xbot-cli command helpers")
-    MyTestCase1.register_subcommand(commands_parser, command_dict1)
-    MyTestCase2.register_subcommand(commands_parser, command_dict2)
-    MyTestCase3.register_subcommand(commands_parser, command_dict3)
-    args = parser.parse_args(["predict", "--batch_size", "12"])
-    assert args.batch_size == 12
+    # list_ = [1, 2, 3]
+    # parse = MyTestCase2.train(command_dict_0, list_)
+    parse1 = MyTestCase2.predict(command_dict_0)
+    assert parse1.seed == 2022
